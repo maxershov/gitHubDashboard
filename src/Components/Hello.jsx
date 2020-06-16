7aa494680f470761f7e088cbfdda4cf3ea191a50
Max Ershov <maxx95.95@gmail.com>
2020-06-16 23:34:03 +0300

    Add my template preact babel webpackDev

---
+++ src/Components/Hello.jsx
@@ -0,0 +1,15 @@
+import React from 'preact/compat';
+
+const Hello = () => {
+
+  return (
+    <>
+    <h1>HELLO</h1>
+    <h2>IT'S ALIVE!!!</h2>
+    <h1>AAAALIVEEEEE!!!</h1>
+    </>
+  );
+}
+
+
+export default Hello;
