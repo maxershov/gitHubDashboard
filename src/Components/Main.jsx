d7333e58c7b20bfd12af987c95fca999ce6c3a25
Max Ershov <maxx95.95@gmail.com>
2020-06-17 00:59:51 +0300

    Add Repository comp

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'preact/compat';
+import Repo from './Repository';
+
 
 const Main = () => {
   const [loading, setLoading] = useState(true);
-
+  const [repos, setRepos] = useState([]);
   useEffect(() => {
     // fetch(`https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&page=1&per_page=5`)
-    fetch(`http://localhost:6701/getData`)
+    const repos = fetch(`http://localhost:6701/getData`)
       .then(res => res.json())
       .then(
         data => {
-          console.log(data);
+          // console.log(data);
           setLoading(false);
+          setRepos(data.data.items);
         },
         err => {
           console.log(err)
@@ -20,7 +23,11 @@ const Main = () => {
   }, [])
   return loading ? <h1>LOADING</h1> : (
     <>
+      {console.log(repos)}
       <h1>Main Page</h1>
+      {repos.map((repo) => {
+        return <Repo name={repo.name} url={repo.html_url} stars={repo.stargazers_count} updated={repo.updated_at} />
+      })}
     </>
   );
 }
---
+++ src/Components/Repository.jsx
@@ -0,0 +1,17 @@
+import React from 'preact/compat';
+
+const Repository = (props) => {
+  const { name, url, stars, updated } = props;
+  console.log(name);
+  return (
+    <>
+      <h1>{name}</h1>
+      <h1>{url}</h1>
+      <p>{stars}</p>
+      <p>{updated}</p>
+    </>
+  );
+}
+
+
+export default Repository;
