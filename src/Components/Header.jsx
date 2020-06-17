bc8d623dee127e13b7b14efad16140425242cbec
Max Ershov <maxx95.95@gmail.com>
2020-06-17 22:31:13 +0300

    Chg header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'preact/compat';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import githubLogo from '../assets/github.svg';
+
+import "./Header.css";
 
 const Header = () => {
 
   return (
-    <nav>
-      <NavLink activeClassName="menu-nav-active" to="/main/">Main</NavLink>
-      <NavLink activeClassName="menu-nav-active" to="/card">Card</NavLink>
-    </nav>
+    <header className="header">
+      <Link to="/main" className="header__link">
+        <img className="header__logo" alt="githubLogo" src={githubLogo} />
+      </Link>
+      <h1 className="header__text">GitHub API</h1>
+    </header>
   );
 }
 
