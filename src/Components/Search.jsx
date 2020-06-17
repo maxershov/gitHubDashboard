be230c218099faa8767dc6d98c302dc939d5e0a4
Max Ershov <maxx95.95@gmail.com>
2020-06-18 00:10:39 +0300

    Add AUTO search with timeout

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,25 +1,45 @@
-import React, { createRef } from 'preact/compat';
+
+import React, { useRef, useState } from 'preact/compat';
 import { useHistory } from 'react-router-dom';
 import useQueryGetter from './useQueryGetter';
-
 import './Search.css';
 
 
 const Search = () => {
   // https://api.github.com/search/repositories?q=tetris&sort=stars
   const { pageNum } = useQueryGetter();
+  const [timeoutFetch, setTimeoutFetch] = useState();
   const history = useHistory();
-  const inputRef = createRef();
+  const [searchQuery, setSearchQuery] = useState();
+
+  // Fix right value in setTimeout closure
+  const inputRef = useRef(searchQuery);
+  inputRef.current = searchQuery;
+
+
+  function handleInput(e) {
+    e.preventDefault();
+    setSearchQuery(e.target.value);
+
+    if (timeoutFetch) {
+      clearTimeout(timeoutFetch);
+    }
+    setTimeoutFetch(setTimeout(() => {
+      history.push(`?search=${inputRef.current}&page=${pageNum}`);
+    }, 1000));
+  }
+
 
   function handleSubmit(e) {
     e.preventDefault();
-    history.push(`?search=${inputRef.current.value}&page=${pageNum}`);
+    clearTimeout(timeoutFetch);
+    history.push(`?search=${searchQuery}&page=${pageNum}`);
   }
+
   return (
     <div className="search">
       <form className="search__form" onSubmit={handleSubmit}>
-        <input className="search__input" type="text" placeholder="Repository name" ref={inputRef} />
-        <button className="search__btn" type="submit">SEARCH</button>
+        <input onChange={handleInput} className="search__input" type="text" placeholder="SEARCH" value={searchQuery} />
       </form>
     </div>
   );
