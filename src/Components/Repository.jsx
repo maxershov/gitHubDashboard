868e8454e68ac524983f09d16ed4a9649e1df05d
Max Ershov <maxx95.95@gmail.com>
2020-06-17 12:16:16 +0300

    Update Repository.jsx

--- src/Components/Repository.jsx
+++ src/Components/Repository.jsx
@@ -1,10 +1,11 @@
 import React from 'preact/compat';
+import { Link } from 'react-router-dom';
 
 const Repository = (props) => {
-  const { name, url, stars, updated } = props;
+  const { id, name, url, stars, updated } = props;
   return (
     <>
-      <h1>{name}</h1>
+      <Link to={`/card/${id}`}>{name}</Link>
       <h1>{url}</h1>
       <p>{stars}</p>
       <p>{updated}</p>
