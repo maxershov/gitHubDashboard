99fb54c32f4e3ae498ef8546ff6adcda708f61b5
Max Ershov <maxx95.95@gmail.com>
2020-06-18 21:52:54 +0300

    Fix 403 err in Linux repo fetch

--- src/fetchAsync.js
+++ src/fetchAsync.js
@@ -1,4 +1,7 @@
 export default async function fetchAsync(url) {
     const response = await fetch(url);
+
+    /* kludge to fix error contributors fetch in linux repo with */
+    if (response.status === 403) return []
     return response.json();
 }
